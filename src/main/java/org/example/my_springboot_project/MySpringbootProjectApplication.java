7a7e0a4be1d258465c5cddd6fc49a04edc83f815
fengzeyuan <2627572328@qq.com>
2025-02-23 22:28:05 +0800

    代码初始化

---
+++ src/main/java/org/example/my_springboot_project/MySpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.my_springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/my_springboot_project/MySpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.my_springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
